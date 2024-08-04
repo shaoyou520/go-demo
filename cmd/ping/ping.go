e89c2afab80020f13b396609a7d09d93b82f7c10
qintao <670026955@qq.com>
2024-08-04 10:15:15 +0800

    监控本地ping连接

--- cmd/ping/ping.go
+++ cmd/ping/ping.go
@@ -1,15 +1,19 @@
-package ping
+package main
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"golang.org/x/net/icmp"
+	"net"
 )
 
-var pingCmd = &cobra.Command{
-	Use:   "ping",
-	Short: "ping example",
-	Long:  `this is ping example`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run hugo...")
-	},
+func main() {
+	netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
+	conn, _ := net.ListenIP("ip4:icmp", netaddr)
+	for {
+		buf := make([]byte, 1024)
+		n, addr, _ := conn.ReadFrom(buf)
+		msg, _ := icmp.ParseMessage(1, buf[0:n])
+		fmt.Println(n, addr, msg.Type, msg.Code, msg.Checksum)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,14 @@
-module "github.com/"
+module github.com/shaoyou520/go-demo
 
 go 1.22
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/net v0.27.0
+)
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
